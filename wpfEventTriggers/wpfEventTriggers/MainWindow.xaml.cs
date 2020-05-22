7f86cb398b220d4e722024f22ead9bc7a6d5aa61
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-22 14:08:41 -0700

    password box

--- wpfEventTriggers/wpfEventTriggers/MainWindow.xaml.cs
+++ wpfEventTriggers/wpfEventTriggers/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace wpfEventTriggers
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Welcome " + tbUsername.Text);
+        }
     }
 }
